919ded0a859af6627ea91733058509a0ad7adc66
Byron Price <byron.h.price@gmail.com>
2018-04-21 16:47:09 -0400

    faster implementation of swishprime

--- SwishPrime.m
+++ SwishPrime.m
@@ -2,8 +2,8 @@ function [y] = SwishPrime(Z)
 %SwishPrime.m
 % Calculate the derivative of the Swish function for an input
 %  fix the tunable parameter beta to be 1
-fx = Swish(Z);
-y = fx+(1+exp(-Z)).^(-1).*(1-fx);
+sigmoid = (1+exp(-Z)).^(-1);
+y = Z.*sigmoid+sigmoid.*(1-Z.*sigmoid);
 end
 
 
