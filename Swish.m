d0557ff1fc5390b88c1b79f6b1a362a6fc485334
Byron Price <byron.h.price@gmail.com>
2018-04-21 16:32:59 -0400

    change evaluation of swish

--- Swish.m
+++ Swish.m
@@ -4,5 +4,5 @@ function [y] = Swish(Z)
 %     Brain
 % Calculate the Swish function for an input
 %  fix the tunable parameter beta to be 1
-y = Z.*(1./(1+exp(-Z))); % beta would be multiplied by Z in the sigmoid
+y = Z.*((1+exp(-Z)).^(-1)); % beta would be multiplied by Z in the sigmoid
 end
--- SwishPrime.m
+++ SwishPrime.m
@@ -2,7 +2,8 @@ function [y] = SwishPrime(Z)
 %SwishPrime.m
 % Calculate the derivative of the Swish function for an input
 %  fix the tunable parameter beta to be 1
-y = Z.*(1./(1+exp(-Z)))+(1./(1+exp(-Z))).*(1-Z.*(1./(1+exp(-Z))));
+fx = Swish(Z);
+y = fx+(1+exp(-Z)).^(-1).*(1-fx);
 end
 
 
